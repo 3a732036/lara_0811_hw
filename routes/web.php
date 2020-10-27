06fcfd530c8e675a8f6b61818641fbd2153ce011
Jun-You <3a732036@gm.student.ncut.edu.tw>
2020-10-28 07:37:46 +0800

    將每一個 Route 都對應到一個獨立的 Controller的 index方法。

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',     [HomeController::class, 'index'])       ->name('home.index');
+Route::get('about', [AboutController::class, 'index'])      ->name('about.index');
+Route::get('news',  [NewsController::class, 'index'])       ->name('news.index');
